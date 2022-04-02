1ca1e0fe7b673f761231d648b113505e8a00a13f
YeonCheol <yeoncheol.jang@gmail.com>
2022-04-03 00:43:04 +0900

    HelloController 추가합니다.

--- src/test/kotlin/com/example/kotlinspringpractice/KotlinSpringPracticeApplicationTests.kt
+++ src/test/kotlin/com/example/kotlinspringpractice/KotlinSpringPracticeApplicationTests.kt
@@ -1,13 +1,24 @@
 package com.example.kotlinspringpractice
 
+import com.example.kotlinspringpractice.controller.HelloController
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@SpringBootTest
-class KotlinSpringPracticeApplicationTests {
+@WebMvcTest(HelloController::class)
+class KotlinSpringPracticeApplicationTests(@Autowired val mockMvc: MockMvc) {
 
     @Test
-    fun contextLoads() {
-    }
+    fun Should_Be_Get_Hello_World() {
+        // when
+        val actual = mockMvc.perform(get("/hello/world"))
 
+        // then
+        actual.andExpect(status().isOk)
+            .andExpect(content().string("Hello World"))
+    }
 }
